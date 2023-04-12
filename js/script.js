bc303fb3adb204229de466445439529d49dd49a6
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-04-12 17:10:30 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah Hasan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a pyramid
+ */
+function calculate () {
+  // input
+  const f = parseInt(document.getElementById('Fahrenheit').value)
+
+  // process
+  const c = (f - 32) * 5 / 9
+
+  // output
+  document.getElementById('Celsius').innerHTML = `The Temperature is: ${c.toFixed(2)} °C`
+}
